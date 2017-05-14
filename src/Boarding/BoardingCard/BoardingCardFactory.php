bf7e3ad1431f3fbf2b311628ccbfeb74f2fea9b5
ariful <neconest@gmail.com>
2017-05-14 21:41:26 +0200

    card factory add

---
+++ src/Boarding/BoardingCard/BoardingCardFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Boarding\BoardingCard;
+
+class BoardingCardFactory
+{
+    public static function build($boardingData)
+    {
+        switch ($boardingData->type) {
+            case BoardingType::AIRPORT_BUS:
+                return new AirportBusBoardingCard(
+                    $boardingData->departure,
+                    $boardingData->destination,
+                    $boardingData->seat
+                );
+            case BoardingType::FLIGHT:
+                return new FlightBoardingCard(
+                    $boardingData->departure,
+                    $boardingData->destination,
+                    $boardingData->seat,
+                    $boardingData->flight,
+                    $boardingData->gate,
+                    $boardingData->counter
+                );
+            case BoardingType::TRAIN:
+                return new TrainBoardingCard(
+                    $boardingData->departure,
+                    $boardingData->destination,
+                    $boardingData->seat,
+                    $boardingData->trainCode
+                );
+            default:
+                throw new \Exception('boarding card type not found', $boardingData->type);
+        }
+    }
+}
---
+++ src/Boarding/BoardingCard/BoardingType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Boarding\BoardingCard;
+
+abstract class BoardingType
+{
+    const TRAIN = 'train';
+    const FLIGHT = 'flight';
+    const AIRPORT_BUS = 'airport_bus';
+}
